fdb4a4d1183ed3384581b1c71bca52fc4f52dfe8
kassio03 <email@address.comkassioferreira0324@gmail.com>
2023-09-16 15:58:56 -0300

    fix: unique fields

--- src/config/database/migrations/1694609243490-first.ts
+++ src/config/database/migrations/1694609243490-first.ts
@@ -5,6 +5,15 @@ export class First1694609243490 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `CREATE TABLE "state" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "uf" character varying NOT NULL, CONSTRAINT "UQ_b2c4aef5929860729007ac32f6f" UNIQUE ("name"), CONSTRAINT "UQ_5c5dcf2d3a6675ddbc6cd3afd55" UNIQUE ("uf"), CONSTRAINT "PK_549ffd046ebab1336c3a8030a12" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "city" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "stateId" uuid NOT NULL, CONSTRAINT "PK_b222f51ce26f7e5ca86944a6739" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "address" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "complement" character varying, "houseNumber" character varying NOT NULL, "cep" character varying NOT NULL, "cityId" uuid NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d92de1f82754668b5f5f5dd4fd5" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
       `CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
@@ -14,13 +23,10 @@ export class First1694609243490 implements MigrationInterface {
       `CREATE TABLE "event" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "description" character varying NOT NULL, "remainingTickets" integer NOT NULL, "totalTickets" integer NOT NULL, "startsAt" TIMESTAMP NOT NULL DEFAULT now(), "finishAt" TIMESTAMP NOT NULL DEFAULT now(), "price" double precision NOT NULL, "poster" character varying NOT NULL, "addressId" uuid NOT NULL, "authorId" uuid NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, CONSTRAINT "PK_30c2f3bbaf6d34a55f8ae6e4614" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "address" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "complement" character varying, "houseNumber" character varying NOT NULL, "cep" character varying NOT NULL, "cityId" uuid NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d92de1f82754668b5f5f5dd4fd5" PRIMARY KEY ("id"))`,
+      `ALTER TABLE "city" ADD CONSTRAINT "FK_e99de556ee56afe72154f3ed04a" FOREIGN KEY ("stateId") REFERENCES "state"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
-      `CREATE TABLE "state" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "uf" character varying NOT NULL, CONSTRAINT "UQ_b2c4aef5929860729007ac32f6f" UNIQUE ("name"), CONSTRAINT "UQ_5c5dcf2d3a6675ddbc6cd3afd55" UNIQUE ("uf"), CONSTRAINT "PK_549ffd046ebab1336c3a8030a12" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `CREATE TABLE "city" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "stateId" uuid NOT NULL, CONSTRAINT "PK_b222f51ce26f7e5ca86944a6739" PRIMARY KEY ("id"))`,
+      `ALTER TABLE "address" ADD CONSTRAINT "FK_3624b3085165071df70276a4000" FOREIGN KEY ("cityId") REFERENCES "city"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
       `ALTER TABLE "ticket" ADD CONSTRAINT "FK_cb22a51617991265571be41b74f" FOREIGN KEY ("eventId") REFERENCES "event"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
@@ -34,21 +40,9 @@ export class First1694609243490 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "event" ADD CONSTRAINT "FK_c180b5b1724f7dd9f6d50915c79" FOREIGN KEY ("authorId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     );
-    await queryRunner.query(
-      `ALTER TABLE "address" ADD CONSTRAINT "FK_3624b3085165071df70276a4000" FOREIGN KEY ("cityId") REFERENCES "city"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "city" ADD CONSTRAINT "FK_e99de556ee56afe72154f3ed04a" FOREIGN KEY ("stateId") REFERENCES "state"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "city" DROP CONSTRAINT "FK_e99de556ee56afe72154f3ed04a"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "address" DROP CONSTRAINT "FK_3624b3085165071df70276a4000"`,
-    );
     await queryRunner.query(
       `ALTER TABLE "event" DROP CONSTRAINT "FK_c180b5b1724f7dd9f6d50915c79"`,
     );
@@ -61,11 +55,17 @@ export class First1694609243490 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "ticket" DROP CONSTRAINT "FK_cb22a51617991265571be41b74f"`,
     );
-    await queryRunner.query(`DROP TABLE "city"`);
-    await queryRunner.query(`DROP TABLE "state"`);
-    await queryRunner.query(`DROP TABLE "address"`);
+    await queryRunner.query(
+      `ALTER TABLE "address" DROP CONSTRAINT "FK_3624b3085165071df70276a4000"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "city" DROP CONSTRAINT "FK_e99de556ee56afe72154f3ed04a"`,
+    );
     await queryRunner.query(`DROP TABLE "event"`);
     await queryRunner.query(`DROP TABLE "ticket"`);
     await queryRunner.query(`DROP TABLE "user"`);
+    await queryRunner.query(`DROP TABLE "address"`);
+    await queryRunner.query(`DROP TABLE "city"`);
+    await queryRunner.query(`DROP TABLE "state"`);
   }
 }
